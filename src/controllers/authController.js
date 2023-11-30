07b6effc247adfd30308005a8da98646fbdd50eb
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-11-29 23:25:05 -0300

    feat: add bycrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const connection = require('../database/database');
+const bcrypt = require('bcrypt');
 
 async function createUser(req, res) {
   const { username, email, age, password } = req.body;
@@ -7,12 +8,19 @@ async function createUser(req, res) {
     return res.status(400).json({ error: 'Campos obrigatórios ausentes' });
   }
 
+  const hashPassword = await bcrypt.hash(password, 10);
+
   const sql = 'INSERT INTO users (username, email, age, password) VALUES (?, ?, ?, ?)';
-  const values = [username, email, age, password];
+  
+  const newUser = {
+    name: username,
+    email: email,
+    age: age,
+};
 
   try {
-    const results = await queryPromise(sql, values);
-    res.status(201).json({ message: 'Usuário criado com sucesso' });
+    const results = await queryPromise(sql, [username, email, age, hashPassword]);
+    res.status(201).json({ message: 'Usuário criado com sucesso', newUser });
   } catch (error) {
     console.error('Erro ao criar usuário:', error);
     res.status(500).json({ error: 'Erro interno ao criar usuário' });
