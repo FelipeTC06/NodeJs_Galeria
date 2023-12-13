01c218b4f1ff1f8035211576205a749b4c52f426
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-12-12 21:46:18 -0300

    feat: save image with userId on database

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,36 +1,40 @@
+const pool = require('../database/database');
 const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path');
 
 class Photo {
-    getPhoto(req, res) {
-        console.log('test');
-        res.send('All photos');
+  async getPhoto(req, res) {
+    console.log('test');
+    res.send('All photos');
+  }
+
+  async sendPhoto(req, res) {
+    try {
+      const { name, userId, details } = req.body;
+
+      const compressedBuffer = await sharp(req.file.buffer)
+        .resize({ width: 10, height: 10 })
+        .toBuffer();
+
+      const base64CompressedImage = compressedBuffer.toString('base64');
+
+      const insertQuery = `
+        INSERT INTO photos (userId, name, details, photo)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
+      `;
+
+      const values = [userId, name, details, base64CompressedImage];
+      const result = await pool.query(insertQuery, values);
+
+      res.status(200).json({
+        result: result.rows[0],
+        message: 'Image saved successfully!'
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error processing or saving the image.');
     }
-
-    sendPhoto(req,res) {
-        sharp(req.file.buffer)
-          .resize({ width: 100, height: 100 })
-          .toBuffer()
-          .then((compressedBuffer) => {
-            const base64CompressedImage = compressedBuffer.toString('base64');      
-            const outputPath = `D:/0 - Projects/Back-End/galeria/photos-galery/${req.file.originalname}`;
-            const outputDirectory = path.dirname(outputPath);
-
-            if (!fs.existsSync(outputDirectory)) {
-              fs.mkdirSync(outputDirectory, { recursive: true });
-            }
-            fs.writeFileSync(outputPath, base64CompressedImage);
-            
-            res.status(200).send(`Image sent, processed and saved successfully in: ${outputPath}`);
-            
-          })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error processing the image.');
-          });
-    }
-
+  }
 }
 
 const photoController = new Photo();
